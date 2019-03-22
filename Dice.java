f085a7be49f3c44bf3d4b93fd4a6ac410cfb67d1
Sayan Ghosh <sgic2001@gmail.com>
2019-03-22 22:47:28 +0530

    Added files

---
+++ Dice.java
@@ -0,0 +1,35 @@
+//Dice Rolling Simulator
+
+class Dice{
+	public static void main(String[] args) {
+
+		int num = (int)(Math.random()*6)+1;
+		int[][] comb = {{},{4,4},{2,2,6,6},{2,6,4,4,6,2},{2,2,2,6,6,2,6,6},{2,2,2,6,6,2,6,6,4,4},{2,2,2,6,4,2,4,6,6,2,6,6}};
+		int[] pat = comb[num]; 
+		for(int i = 0;i < 9;i++){
+			for(int j =0 ; j < 9;j++){
+
+				if(i==0||i==8){
+					System.out.print("-");
+				}
+				else if ( j == 0 || j == 8 ){
+					System.out.print("|");
+				}
+				else {
+					boolean p = false;
+					for(int k = 0; k < pat.length;k+=2){
+						if (i==pat[k]&&j==pat[k+1]){
+							System.out.print(".");
+							p = true;
+							break;
+						}
+					}
+					if(!p)
+						System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.print("\n"+num);
+	}
+}
